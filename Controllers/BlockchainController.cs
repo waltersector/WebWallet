1d330a4eecf6648049073bba3f4f0187574fdb5e
Dave <dave@thisisembrace.com>
2018-09-15 09:23:32 +0200

    Project restructure to dotnetcore

---
+++ Controllers/BlockchainController.cs
@@ -0,0 +1,48 @@
+﻿using LiteDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebWallet.Helpers;
+using WebWallet.Models;
+
+namespace WebWallet.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlockchainController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult Get(int height = 0)
+        {
+            var startHeight = height;
+            var endHeight = startHeight + 100;
+            if (startHeight < 1) startHeight = 1;
+            var chainHeight = RpcHelper.Request<GetHeightResp>("getheight").Height;
+            if (chainHeight == 0) chainHeight = endHeight + 100;
+            if (endHeight > chainHeight)
+                endHeight = chainHeight;
+            //used to pick the correct file
+            var start = Convert.ToInt32(Math.Floor((double)(height / 10000) * 10000)) + 1;
+            var end = start + 10000 - 1;
+
+            try
+            {
+                using (var db = new LiteDatabase(string.Concat(AppContext.BaseDirectory, @"App_Data/", "transactions_", start, "-", end, ".db")))
+                {
+                    var transactions = db.GetCollection<CachedTx>("cached_txs");
+                    var txs = transactions.Find(x => x.height >= startHeight && x.height <= endHeight);
+                    return new JsonResult(JsonConvert.SerializeObject(txs));
+                }
+            }
+            catch (Exception ex)
+            {
+                //todo: log and return client handlable exception
+            }
+
+            return new JsonResult("");
+        }
+    }
+}
--- src_api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$testnet = false;
-$cacheLocation = $testnet ? 'cache-testnet' : 'cache';
-$daemonAddress = '127.0.0.1';
-$rpcPort = $testnet ? 11898 : 11898;
-$coinSymbol = 'trtl';
