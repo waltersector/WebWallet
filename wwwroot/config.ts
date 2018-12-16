8217debfcee6ec40f561ad2e3e8956f3d75f153c
Silly Ghost <34451068+sillyghost@users.noreply.github.com>
2018-12-16 15:39:33 +0530

    Update config.ts

--- wwwroot/config.ts
+++ wwwroot/config.ts
@@ -1,13 +1,13 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
-    apiUrl: typeof window !== 'undefined' && window.location ? window.location.href.substr(0, window.location.href.lastIndexOf('/') + 1) + 'api/' : 'https://wallet.plenteum.com/api/',
-    mainnetExplorerUrl: "https://turtle.land",
-    testnetExplorerUrl: "https://explorer.turtlecoin.lol/",
+    apiUrl: typeof window !== 'undefined' && window.location ? window.location.href.substr(0, window.location.href.lastIndexOf('/') + 1) + 'api/' : 'http://mrdigicoin.eu/wallet/api/',
+    mainnetExplorerUrl: "http://mrdigicoin.eu/explorer/",
+    testnetExplorerUrl: "http://mrdigicoin.eu/explorer/",
     testnet: false,
-    coinUnitPlaces: 2,
+    coinUnitPlaces: 4,
     txMinConfirms: 60,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
     txCoinbaseMinConfirms: 120, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-    addressPrefix: 3914525,
+    addressPrefix: 0x1b40c8,
     integratedAddressPrefix: 0,
     addressPrefixTestnet: 0,
     integratedAddressPrefixTestnet: 0,
@@ -15,14 +15,14 @@ global.config = {
     subAddressPrefixTestnet: 0,
     feePerKB: new JSBigInt('100'),//20^10 - for testnet its not used, as fee is dynamic.
     dustThreshold: new JSBigInt('1'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
-    defaultMixin: 7, // default value mixin
+    defaultMixin: 2, // default value mixin
     idleTimeout: 30,
     idleWarningDuration: 20,
 
-    coinSymbol: 'TRTL',
-    openAliasPrefix: "trtl",
-    coinName: 'TurtleCoin',
-    coinUriPrefix: 'turtlecoin:',
-    avgBlockTime: 30,
+    coinSymbol: 'AYA',
+    openAliasPrefix: "aya",
+    coinName: 'AryaCoin',
+    coinUriPrefix: 'aryacoin:',
+    avgBlockTime: 300,
     maxBlockNumber: 500000000,
 };
