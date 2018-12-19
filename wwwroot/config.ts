040a7c7b6b0e1301af4b9d3910efb47ed52c02ec
Silly Ghost <34451068+sillyghost@users.noreply.github.com>
2018-12-19 20:06:56 +0530

    Update config.ts

--- wwwroot/config.ts
+++ wwwroot/config.ts
@@ -1,7 +1,7 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
     apiUrl: typeof window !== 'undefined' && window.location ? window.location.href.substr(0, window.location.href.lastIndexOf('/') + 1) + 'api/' : '',
-    mainnetExplorerUrl: "http://mrdigicoin.eu/explorer/",
+    mainnetExplorerUrl: "127.0.0.1",
     testnetExplorerUrl: "http://mrdigicoin.eu/explorer/",
     testnet: false,
     coinUnitPlaces: 4,
@@ -13,7 +13,7 @@ global.config = {
     integratedAddressPrefixTestnet: 0,
     subAddressPrefix: 0,
     subAddressPrefixTestnet: 0,
-    feePerKB: new JSBigInt('5'),//20^10 - for testnet its not used, as fee is dynamic.
+    feePerKB: new JSBigInt('10'),//20^10 - for testnet its not used, as fee is dynamic.
     dustThreshold: new JSBigInt('1'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
     defaultMixin: 7, // default value mixin
     idleTimeout: 30,
